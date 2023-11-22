486726527ed06125e27250dd826492b31a30f4ff
mcnamee98 <jmcnamee@andrew.cmu.edu>
2023-11-22 13:16:22 -0500

    Comments added for all of StepI
    We now have comments for all of StepI. Additionally, a minor revision was made in this step of commenting out 'EEG_mask' which was not used

--- CSD_SD_II_StepI_vF.m
+++ CSD_SD_II_StepI_vF.m
@@ -47,55 +47,73 @@ for ss = 1:Sessions_size
     % Filter EEG data to extract the delta frequency band 
     EEG_vis = pop_eegfiltnew(EEG_vis, 0.5,4,10000,0,[],1);
     
+    % Extract ECoG channels from EEG struct and save as "EEG_sub"
     EEG_sub = EEG_vis.data(end-5:end,:);
     Limit_vec = ones(size(EEG_sub,1),2);%[1e5,1.5e5];
     Limit_vec(:,2) = Limit_vec(:,2)*size(EEG_sub,2);
     
-    EEG_data = EEG_sub;
-    EEG_temp = EEG_sub;
-    Delete_ind_tot = struct;
+    % Save copies of ECoG data
+    EEG_data = EEG_sub; % Copy for masking 
+    EEG_temp = EEG_sub; % Copy for plotting pre outlier removed data
+    Delete_ind_tot = struct; % Initialize a strct for bad indicies
+
+    % Loop through each of the 6 ECoG channels
     for j=1:6
-        j
-        temp = EEG_sub(j,EEG_sub(j,:)~=0);
+        j;
+        % Create data array of all non-zero data points at channel j 
+        temp = EEG_sub(j,EEG_sub(j,:)~=0); 
+        % Find indices of EEG sub which are not zero
         TF_temp = find(EEG_sub(j,:)~=0);
+        % Find outliers 10 IQRs above the upper quartile of the data
         [~,TF] = rmoutliers(temp,'quartiles','ThresholdFactor',10);
+        % Map outlier indices back to non-zeroed space
         T_ind = TF_temp(TF);
         Delete_ind = [];
         Delete_ind = cat(2,Delete_ind,T_ind);
+        % Store channel specific mask indices in Session wide struct
         Delete_ind_tot(j).Delete = unique(Delete_ind);
     end
     
-    
+    % Set window length for outlier removal padding  
     w_length = 100;
-    for j=1:6
+    for j=1:6 % Loop through each channel 
+        % Removal window lengths before and after each outlier index
         for i=-w_length:w_length
             Delete_ind_delayed = Delete_ind_tot(j).Delete+i;
+            % Ensure no delayed index's are negative or above upper limit
             Delete_ind_delayed(Delete_ind_delayed<=0) = [];
             Delete_ind_delayed(Delete_ind_delayed>size(EEG_data,2)) = [];
+            % Remove outlier delayed indices
             EEG_data(j,Delete_ind_delayed) = 0;
-            EEG_mask(j,Delete_ind_delayed) = 0;
+            % EEG_mask(j,Delete_ind_delayed) = 0;
         end
     end
     
-    EEG_sub = EEG_data(:,:);
+    EEG_sub = EEG_data(:,:); % Assign masked EEG_data back to EEG_sub
     
-    close all
+    close all % Call for closing any previous open MATLAB figures
+    % For each channel plot premasked and masked data for visualization
     for i=1:6
-        figure;plot(EEG_temp(i,:))
+        figure;plot(EEG_temp(i,:)) % EEG temp represents 'precleaned' data
         hold on
-        plot(EEG_sub(i,:))
+        plot(EEG_sub(i,:)) % EEG sub represents 'cleaned' data
         hold off
     end
     
-    
+    % Normalize each ECoG channel by channels' standard deviations 
     EEG_sub = EEG_sub./std(EEG_sub,0,2);
+    % Ensure no indices are NaN
     EEG_sub(isnan(EEG_sub)) = 0;
-    
+    % Add ECoG channels back to EEG_vis variable 
     EEG_vis.data(20:25,:) = EEG_sub;
-    
+    % Save EEG_Vis with cleaned and normalized ECoG channels 
     EEG_vis = pop_saveset(EEG_vis, 'filename',[Session_names(ss).name,'_Visualization_Delta_ECoGcleaned_vII.set'],'filepath',current_path);
+    
+    % For visualization purposes only, zero EEG chans and plot only ECoG
     EEG_vis.data(1:19,:) = 0;
     pop_eegplot(EEG_vis, 1, 1, 1);
+
+    % Exit Session Specific directory
     cd ..
 end
 
